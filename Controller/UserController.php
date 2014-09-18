c0d6b0ef83c101c6292dae68756757148aa2d49e
Djuro Mandinic <djuro.mandinic@gmail.com>
2014-09-18 15:49:40 +0200

    Created User document and CRUD actions.

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -4,49 +4,125 @@ namespace Acme\BlogBundle\Controller;
 
 use Acme\BlogBundle\Form\Type\UserType;
 use Acme\BlogBundle\Document\User;
+use Acme\BlogBundle\Form\Model\User as FormUser;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+* @Route("/user")
+*/
 class UserController extends Controller
 {
     
-
 	/**
-     * @Route("/new")
-     * @Template()
-     */
+    * Displays user form and handles form submission.
+    *
+    * @Route("/new", name="blog_user_new")
+    * @Template()
+    */
     public function newAction(Request $request)
     {
+        $formUser = new FormUser();
+
         $form = new UserType();
-        $form = $this->createForm( new UserType());
+
+        $form = $this->createForm(new UserType(), $formUser);
 
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            // spremiti u bazu
-            $data = $form->getData();
+        if ($form->isValid())
+        {
+                        
+            $documentUser = new User();
 
-            $user = new User();
+            $documentUser->setFirstName($formUser->getFirstName());
+            $documentUser->setLastName($formUser->getLastName());
+            $documentUser->setPassword($formUser->getPassword());
+            $documentUser->setEmail($formUser->getEmail());
 
+            $dm = $this->get('doctrine_mongodb')->getManager();
+            $dm->persist($documentUser);
+            $dm->flush();
 
-    $user->setFirstName('Pero');
-    $user->setLastName('Perić');
-
-    $dm = $this->get('doctrine_mongodb')->getManager();
-    $dm->persist($user);
-    $dm->flush();
-            //return $this->redirect($this->generateUrl('task_success'));
+            return $this->redirect($this->generateUrl('blog_user_list'));
 
         }
 
         return array(
             'form' => $form->createView()
             );
-    	//return array('nesto' => "Ovo je kako nekakav tekst za prvu stranicu ...");
+    }
+
+
+    /**
+    *  Lists user data in grid.
+    *
+    * @Route("/list",name="blog_user_list")
+    * @Template()
+    */
+    public function listAction()
+    {
+        $users = $this ->get('doctrine_mongodb')
+                        ->getRepository('AcmeBlogBundle:User')
+                        ->findAll();
+
+        if (!$users) 
+        {
+            throw $this->createNotFoundException('No Users found');
+        }
+
+        return array(
+            'users' => $users
+            );
+    }
+
+
+    /**
+    *  Displays user form for editing and handles form submission.
+    *
+    * @Route("/edit/{userId}",name="blog_user_edit")
+    * @Template()
+    */
+    public function editAction(Request $request, $userId)
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $documentUser = $dm->getRepository('AcmeBlogBundle:User')->find($userId);
+
+        if (!$documentUser)
+        {
+            throw $this->createNotFoundException('No User found having id: '.$userId);
+        }
+
+        $formUser = new FormUser();
+        $formUser->setFirstName($documentUser->getFirstName());
+        $formUser->setLastName($documentUser->getLastName());
+        $formUser->setEmail($documentUser->getEmail());
+
+        $form = new UserType();
+
+        $form = $this->createForm(new UserType(), $formUser);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $documentUser->setFirstName($formUser->getFirstName());
+            $documentUser->setLastName($formUser->getLastName());
+            $documentUser->setEmail($formUser->getEmail());
+
+            $dm->persist($documentUser);
+            $dm->flush();
+
+            return $this->redirect($this->generateUrl('blog_user_list'));
+        }
+
+        return array(
+            'form' => $form->createView()
+            );
     }
 
 
--- Form/Type/UserType.php
+++ Form/Type/UserType.php
@@ -3,6 +3,8 @@ namespace Acme\BlogBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
 
 class UserType extends AbstractType
 {
@@ -13,6 +15,13 @@ class UserType extends AbstractType
             ->add('lastname', 'text',array('required'=>true))
             ->add('email', 'text',array('required'=>true))
             ->add('password', 'text',array('required'=>true));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acme\BlogBundle\Form\Model\User',
+        ));
     }
 
     public function getName()
